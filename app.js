4c0b24c93a5fdb4b00707ca8db6d9342f5211885
Ashish <Ashish@espl.local>
2018-08-20 18:36:10 +0530

    change

--- app.js
+++ app.js
@@ -55,7 +55,8 @@ var referralRouter=require('./routers/referral');
 
 const router = express.Router()
 var app = express();
-const port = config.PORT || 3000;
+// const port = config.PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
--- config/config.js
+++ config/config.js
@@ -1,12 +1,12 @@
 // var env = process.env.NODE_ENV || 'development';
 
-// if (env === 'development') {
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/elaundry';
-// } else if (env === 'test') {
-//   process.env.PORT = 3000;
-//   process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
-// }
+if (env === 'development') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/elaundry';
+} else if (env === 'test') {
+  process.env.PORT = 3000;
+  process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
 
 module.exports = {
   'secret': 'nodeauthsecret',
