24daf9126c7d4d4e4a4ec2dfede1460837ad591d
Ashish <Ashish@espl.local>
2018-08-20 18:28:54 +0530

    change in customer api

--- db/mongoose.js
+++ db/mongoose.js
@@ -12,6 +12,7 @@ mongoose.connect('mongodb://yash:yash123@ds123372.mlab.com:23372/washcare', {
     process.exit();
 });
 
+
 // mongodb://yash:yash123@ds123372.mlab.com:23372/washcare
 // mongodb://localhost:27017/elaundry
 module.exports = { mongoose };
